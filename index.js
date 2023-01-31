41ab8748a307811c950006b7b757b70ca7dbac4e
Muhammad Hassaan Ayaz <m.hassaanayaz04@gmail.com>
2023-01-31 23:51:50 +0500

    Connect database Using nodejs

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
-const express =require("express")
-const app=express()
+const express = require("express")
+const app = express()
 const path = require('path');
-app.use(express.static(path.join(__dirname,'/public')));
+const MongoClient=require('mongodb').MongoClient
+app.use(express.json())
 
 
-app.get('/views/home.html',(req,res)=>{
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+
+    MongoClient.connect('mongodb://0.0.0.0:27017/',{useNewUrlParser:true , useUnifiedTopology: true},function(error,result){
+        if (error )throw error
+        database=result.db("CSE_Management")
+        console.log("CONNECTION")
+        database.collection("admin").findOne({},function(error,result){
+            if (error )throw error;
+            console.log(result);
+            
+        })
+        // console.log(users)
+    })
+
+
+
+
+
+app.get('/views/home.html', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
-app.get('/views/login.html',(req,res)=>{
+app.get('/views/login.html', (req, res) => {
     res.sendFile(__dirname + "/views/login.html")
 })
-app.get('/views/enrolledSts.html',(req,res)=>{
+app.get('/views/enrolledSts.html', (req, res) => {
     res.sendFile(__dirname + "/views/enrolledSts.html")
 })
-app.get('/views/stsDetails.html',(req,res)=>{
+app.get('/views/stsDetails.html', (req, res) => {
     res.sendFile(__dirname + "/views/stsDetails.html")
 })
-app.get('/views/attendance.html',(req,res)=>{
+app.get('/views/attendance.html', (req, res) => {
     res.sendFile(__dirname + "/views/attendance.html")
     res.sendFile(__dirname + "/public/attendance.css")
 })
-app.get('/views/News.html',(req,res)=>{
+app.get('/views/News.html', (req, res) => {
     res.sendFile(__dirname + "/views/News.html")
 })
-app.get('/views/dashboard.html',(req,res)=>{
+app.get('/views/dashboard.html', (req, res) => {
     res.sendFile(__dirname + "/views/dashboard.html")
 })
-app.get('/views/faculty.html',(req,res)=>{
+app.get('/views/faculty.html', (req, res) => {
     res.sendFile(__dirname + "/views/faculty.html")
 })
 
-app.listen(5500,function(){
+app.listen(5500, function () {
     console.log(`Express listening to port 5500`)
 })
