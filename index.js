d6a6c7631251f449cf1efe74b579a6a944589312
saman khalid <samnn2381@yahoo.com>
2023-02-01 10:18:42 +0500

    connect pages

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+const { count } = require("console");
 const express = require("express")
 const app = express()
 const path = require('path');
 const MongoClient=require('mongodb').MongoClient
 app.use(express.json())
-
-
-
+app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname, '/public')));
-
-
-
-
+var totalStds;
+var totalFac;
     MongoClient.connect('mongodb://0.0.0.0:27017/',{useNewUrlParser:true , useUnifiedTopology: true},function(error,result){
         if (error )throw error
         database=result.db("CSE_Management")
@@ -19,41 +16,62 @@ app.use(express.static(path.join(__dirname, '/public')));
         database.collection("admin").findOne({},function(error,result){
             if (error )throw error;
             console.log(result);
-            
         })
-        // console.log(users)
+        let stds=database.collection("students");
+        stds.count().then((count)=>{
+            console.log(count);
+            totalStds=count;            
+        })
+        let fac=database.collection("instructors");
+        fac.count().then((count)=>{
+            console.log(count);
+            totalFac=count;            
+        })
+    
     })
 
-
-
-
-
-app.get('/views/home.html', (req, res) => {
-    res.sendFile(__dirname + "/views/home.html")
-})
-app.get('/views/login.html', (req, res) => {
-    res.sendFile(__dirname + "/views/login.html")
-})
-app.get('/views/enrolledSts.html', (req, res) => {
-    res.sendFile(__dirname + "/views/enrolledSts.html")
-})
-app.get('/views/stsDetails.html', (req, res) => {
-    res.sendFile(__dirname + "/views/stsDetails.html")
-})
-app.get('/views/attendance.html', (req, res) => {
-    res.sendFile(__dirname + "/views/attendance.html")
-    res.sendFile(__dirname + "/public/attendance.css")
-})
-app.get('/views/News.html', (req, res) => {
-    res.sendFile(__dirname + "/views/News.html")
-})
-app.get('/views/dashboard.html', (req, res) => {
-    res.sendFile(__dirname + "/views/dashboard.html")
-})
-app.get('/views/faculty.html', (req, res) => {
-    res.sendFile(__dirname + "/views/faculty.html")
+app.get('/home', (req, res) => {
+    res.render("home",{totalStds:totalStds, totalFac:totalFac})
+    // res.sendFile(__dirname + "/views/home.html")
 })
 
-app.listen(5500, function () {
+// app.get('/views/login.html', (req, res) => {
+app.get('/login', (req, res) => {
+    res.render("login")
+    // res.sendFile(__dirname + "/views/login.html")
+})
+// app.get('/views/enrolledSts.html', (req, res) => {
+app.get('/enrolledSts', (req, res) => {
+    res.render("enrolledSts")
+    // res.sendFile(__dirname + "/views/enrolledSts.html")
+})
+// app.get('/views/stsDetails.html', (req, res) => {
+app.get('/stsDetails', (req, res) => {
+    res.render("stsDetails")
+    // res.sendFile(__dirname + "/views/stsDetails.html")
+})
+// app.get('/views/attendance.html', (req, res) => {
+app.get('/attendance', (req, res) => {
+    res.render("attendance")
+    // res.sendFile(__dirname + "/views/attendance.html")
+    // res.sendFile(__dirname + "/public/attendance.css")
+})
+// app.get('/views/News.html', (req, res) => {
+app.get('/News', (req, res) => {
+    res.render("News")
+    // res.sendFile(__dirname + "/views/News.html")
+})
+// app.get('/views/dashboard.html', (req, res) => {
+app.get('/dashboard', (req, res) => {
+    res.render("dashboard")
+    // res.sendFile(__dirname + "/views/dashboard.html")
+})
+// app.get('/views/faculty.html', (req, res) => {
+app.get('/faculty', (req, res) => {
+    res.render("faculty")
+    // res.sendFile(__dirname + "/views/faculty.html")
+})
+
+app.listen(5800, function () {
     console.log(`Express listening to port 5500`)
 })
